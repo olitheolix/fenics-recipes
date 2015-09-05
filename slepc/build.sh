b1882ba7261821a67c8d48d1495746bda74ebd8c
Oliver Nagy <olitheolix@gmail.com>
2015-09-05 18:44:01 +1000

    bugfix: added PETSc for AWS build problem.

--- slepc/build.sh
+++ slepc/build.sh
@@ -14,7 +14,27 @@ export PETSC_DIR=$PREFIX
 # Calling Python 2 this way is a hack. It will reliably work in an
 # Anaconda2 container, though.
 /opt/conda/bin/python configure --prefix=$PREFIX
-make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX
+
+# The following construct simply calls make until it either compiles
+# successfully, or has failed 10 times. This somewhat peculiar
+# approach is not necessary when I build bets in a Docker container on
+# my local computer, but when I build the exact same container on an
+# AWS instance then the compilation often (but not always) fails. The
+# output suggests there may be a file system race condition during
+# compilation (PETSc builds itself with multiple threads by default).
+# As a workaround, I will simply attempt to call the 'make' command
+# several times. This appears to fix it.
+i="1"
+until make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX
+do
+  sleep 0.5
+  i=$[$i+1]
+  if [ $i -gt 5 ]; then
+      exit 1
+  fi
+done
+echo "It took $i attempts to compile PETSc"
+
 make SLEPC_DIR=$PWD PETSC_DIR=$PREFIX install
 
 # Add more build steps here, if they are necessary.
